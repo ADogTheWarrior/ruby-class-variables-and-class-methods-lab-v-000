61003ff3c9854f17d166291519b55183fef75416
Adam Fudge <coolfudge@gmail.com>
2017-10-14 17:04:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,16 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+
+    #if new, add new thing and put its count at 1, also add it to the array
+    #else += 1 to the count of matching thing
+
+    if genre_count.has_key? genre
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 0
+      genres << genre
+    end
   end
 
   def self.count
