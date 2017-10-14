d3a51033c934f543a12640a480181e6991c1fb11
Adam Fudge <coolfudge@gmail.com>
2017-10-14 17:07:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,11 @@ class Song
     #if new, add new thing and put its count at 1, also add it to the array
     #else += 1 to the count of matching thing
 
-    if genre_count.has_key? genre
-      genre_count[genre] += 1
+    if @@genre_count.has_key? genre
+      @@genre_count[genre] += 1
     else
-      genre_count[genre] = 1
-      genres << genre
+      @@genre_count[genre] = 1
+      @@genres << genre
     end
   end
 
