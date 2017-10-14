ec649d4205db5c7ce8762ea0c0b38027acadfb7c
Adam Fudge <coolfudge@gmail.com>
2017-10-14 16:44:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,15 @@
 class Song
-  @@song_count = 0
+  @@count = 0
+  @@genres = []
   attr_accessor :name
   attr_accessor :artist
   attr_accessor :genre
 
   def initialize(name, artist, genre)
-    @@song_count += 1
+    @@count += 1
   end
 
   def self.count
-    @@album_count
+    @@count
   end
 end
