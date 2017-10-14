1f0d60dad28744db3e3ae74d29697526b9791f3f
Adam Fudge <coolfudge@gmail.com>
2017-10-14 17:06:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
     if genre_count.has_key? genre
       genre_count[genre] += 1
     else
-      genre_count[genre] = 0
+      genre_count[genre] = 1
       genres << genre
     end
   end
